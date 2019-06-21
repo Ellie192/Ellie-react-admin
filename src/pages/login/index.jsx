0b1ec3a4a68750807d29b90a0f0f259e9c969fa1
86186 <lilihuan520@aliyun.com>
2019-06-21 21:00:37 +0800

    login表单验证完成

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -5,10 +5,44 @@ import { Form, Icon, Input, Button, } from 'antd';
 import logo from './logo.png';
 import './index.less';
 
+const Item = Form.Item;
 
-export default class Login extends Component{
+class Login extends Component{
+
+    validator = (rule,value,callback) => {
+        console.log(rule,value);
+
+        ;const name = rule.fullField === 'username' ? "username" :'password';
+
+        if(!value){
+            callback(`${name}不能为空`)
+        }else if(value.length < 4){
+            callback(`${name}不得少于4位`)
+        }else if(value.length > 15){
+            callback(`${name}不得多于15位`)
+        }else if(!/^[a-zA-Z_0-9]+$/.test(value)){
+            callback(`${name}只能包含英文字母，数字，下划线`)
+        }else{
+            callback()
+        }
+    };
+
+    login = (e) => {
+        e.preventDefault();
+
+        this.props.form.validateFields((error,values) => {
+            if(!error){
+                const { username,password } = values;
+                console.log(username,password)
+            }else{
+                console.log('登录表单验证有误')
+            }
+        })
+
+    }
     render(){
-        const Item = Form.Item;
+
+        const { getFieldDecorator } = this.props.form;
         return(
             <div  className="login" >
                 <header className="login-header">
@@ -16,14 +50,41 @@ export default class Login extends Component{
                     <h1>React项目: 后台管理系统</h1>
                 </header>
                 <section className="login-content">
-                    <Form className="login-form">
+                    <Form className="login-form" onSubmit={this.login}>
                         <h2>用户登录</h2>
                         <Item>
-                            <Input className="login-input" prefix={<Icon type="user" />} placeholder='用户名'/>
+                            {
+                                getFieldDecorator('username', {
+                                rules: [
+                                    /*{ required: true, message: '用户名不能为空' },
+                                    {min:4,message:'用户名不得少于四位'},
+                                    {max:15,message:'用户名不得多于十五位'},
+                                    {pattern:/^[a-zA-Z_0-9]+$/,message:'用户名只能包含英文字母，数字，下划线'}*/
+                                    {validator:this.validator}
+                                ],
+                            })(
+                                    <Input className="login-input" prefix={<Icon type="user" />} placeholder='用户名'/>
+                                )
+                            }
+
                         </Item>
                         <Item>
-                            <Input className="login-input"  prefix={<Icon type="lock" />} placeholder='密码' type="password"/>
+                            {
+                                getFieldDecorator(
+                                    'password',
+                                    {rules:[
+                                            /*{ required: true, message: '密码不能为空' },
+                                            {min:4,message:'密码不得少于六位'},
+                                            {max:15,message:'密码不得多于十五位'},
+                                            {pattern:/^[a-zA-Z_0-9]+$/,message:'密码只能包含英文字母，数字，下划线'}*/
+                                            {validator:this.validator}
+                                        ]}
+                                )(
+                                    <Input className="login-input"  prefix={<Icon type="lock" />} placeholder='密码' type="password"/>
+                                )
+                            }
                         </Item>
+
                         <Item>
                             <Button type='primary' htmlType="submit" className="login-btn">登录</Button>
                         </Item>
@@ -34,3 +95,4 @@ export default class Login extends Component{
         )
     }
 }
+export default  Form.create({ })(Login);
